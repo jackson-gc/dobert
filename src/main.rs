027d681c04ccf874765d8fc5bd7b06c6a252b38b
gc <mcdo0724@algonquinlive.com>
2025-03-27 18:08:28 -0400

    depth working

--- src/main.rs
+++ src/main.rs
@@ -25,7 +25,8 @@ struct Rect {
     e_pnt: Point
 }
 
-const MIN_WINDOW_SIZE: u16 = 32;
+const MIN_WINDOW_WIDTH: u16 = 32;
+const MIN_WINDOW_LENGTH: u16 = 32;
 
 
 fn main() -> io::Result<()> {
@@ -41,17 +42,22 @@ fn main() -> io::Result<()> {
 
 
 fn draw_pot(trk: &mut Stdout) -> Result<()> {
-    let window_size: u16 = size()?.0;
-    
-    if window_size < MIN_WINDOW_SIZE {
-        panic!("Window size is too small (must be >32)")
+    let window_width: u16 = size()?.0;
+    let window_length: u16 = size()?.1;
+
+    if window_width < MIN_WINDOW_WIDTH {
+        panic!("Window width is too small (must be >{})", MIN_WINDOW_WIDTH);
     }
 
-    let start_index: u16 = window_size - MIN_WINDOW_SIZE;
-    let margin: u16 = start_index / 4;
-    let size: u16 = window_size - (margin * 2);
+    if window_length < MIN_WINDOW_LENGTH {
+        panic!("Winow length is too small (must be >{})", MIN_WINDOW_LENGTH);
+    }
 
-    println!("start_index: {:?}, size: {:?}, margin: {:?}, total: {:?}",start_index, size, margin, window_size);
+    let start_index: u16 = window_width - MIN_WINDOW_WIDTH;
+    let margin: u16 = start_index / 4;
+    let size: u16 = window_width - (margin * 2);
+    let pot_depth: u16 = window_length / 4;
+    let mut depth: u16 = window_length - pot_depth;
 
     let clr: Color = Color::Rgb{r: 160, g: 130, b: 90};
     let pot_hard_mat: StyledContent<char> = '█'.with(clr);
@@ -59,19 +65,17 @@ fn draw_pot(trk: &mut Stdout) -> Result<()> {
     paint_rect(trk, pot_hard_mat, Rect {
         s_pnt: Point {
             x: margin,
-            y: 20
+            y: depth
         },
         e_pnt: Point {
             x: margin + size,
-            y: 20 + 1
+            y: depth + 1
         }
     })?;
 
-
-
     let mut draw_pnt = Point {
         x: margin + (margin / 4),
-        y: 21
+        y: depth
     };
 
     let mut draw_shift = Shifter {
@@ -80,12 +84,26 @@ fn draw_pot(trk: &mut Stdout) -> Result<()> {
         y_shift: 1
     };
     
-    for _i in 1..12 {
+    for i in 1..pot_depth {
+        if i & 2 == 0 {
+            draw_shift.x_shift = 0;
+        } else {
+            draw_shift.x_shift = 1;
+        }
+
         paint_outline(trk, pot_hard_mat, &mut draw_pnt, &mut draw_shift)?;
     }
 
 
-
+    let end_width: u16 = draw_pnt.x + draw_shift.gap + 1;
+    let end_length: u16 = draw_pnt.y;
+    paint_rect(trk, pot_hard_mat, Rect {
+        s_pnt: draw_pnt,
+        e_pnt: Point {
+            x: end_width,
+            y: end_width
+        }
+    });
 
     Ok(())
 
