31b0058348ba3db366d76e63f637702aa5288561
Jackson McDonald <mcdo0724@algonquinlive.com>
2025-03-29 02:28:06 +0000

    started work on branches

--- src/plant.rs
+++ src/plant.rs
@@ -6,21 +6,25 @@ use crossterm::style::Stylize;
 use crate::utils::draw_utils::{Renderer, Point, Token};
 
 pub fn plant(renderer: &mut Renderer, seed: u64) -> Result<()> {
-    let window_size = renderer.get_window_ctx();
     let mut rng = ChaCha8Rng::seed_from_u64(seed);
+    draw_branch(renderer, &mut rng, 16)?;
     
-    // Fix deprecated method - use random_range instead of gen_range
-    let point: u16 = rng.random_range(1..window_size.0);
     
-    println!("{}/{}", point, window_size.0);
-
-
-
-    let cursor: Token = '#'.red();
-    renderer.paint_tile(cursor, Point {
-        x: point,
-        y: 20
-    })?;
-
     Ok(()) 
+}
+
+
+
+fn draw_branch(renderer: &mut Renderer, rng: &mut ChaCha8Rng, stems: u16) -> Result<()> {
+    let window_size = renderer.get_window_ctx();
+    let tip: Token = '%'.dark_green();
+    
+    for _ in 0..stems{
+        let _ = renderer.paint_tile(tip, Point {
+            x: rng.random_range(5..window_size.0 - 5),
+            y: rng.random_range(5..(window_size.1 - (window_size.1 / 4)))
+        });
+    };
+
+    Ok(())
 }
