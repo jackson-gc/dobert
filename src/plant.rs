81fedc4f322292c8b570657c9bcf2a1ca88bed07
Jackson McDonald <mcdo0724@algonquinlive.com>
2025-04-11 14:25:36 -0400

    restrucuted branch logic (again)

--- src/plant.rs
+++ src/plant.rs
@@ -24,41 +24,46 @@ impl Plant {
 }
 
 struct Branch {
-    nut: Point,
     tip: Point
 }
 
 
 pub fn pot_the_plant(renderer: &mut Renderer, plant: &mut Plant) -> Result<()> {
-    
     let window_size = renderer.window_size;
 
     plant.nut.x = window_size.0 / 2;
     plant.nut.y = window_size.1 - (window_size.1 / 8) - 1;
     renderer.draw_tile('#'.red(), &plant.nut)?;
 
-    let _all_tips: Vec<Point> = find_tips(renderer, &mut plant.rng);
+    plant.branches = gen_branches(renderer, &mut plant.rng);
+    
+    draw_branches(renderer, plant)?;
     Ok(())
 }
 
-fn draw_branches(renderer: &mut Renderer, rng: &mut ChaCha8Rng){
+fn draw_branches(renderer: &mut Renderer, plant: &mut Plant) -> Result<()> {
+    
 
+
+
+    Ok(())
 }
 
 
-fn find_tips(renderer: &mut Renderer, rng: &mut ChaCha8Rng) -> Vec::<Point> {
+fn gen_branches(renderer: &mut Renderer, rng: &mut ChaCha8Rng) -> Vec::<Branch> {
     let tip_mat: Token = '%'.dark_green();
     let ws = renderer.window_size;
-    let mut all_tips = Vec::<Point>::new();
+    let mut branches = Vec::<Branch>::new();
     let max_x = ws.0 - 5;
     let max_y = ws.1 - (ws.1 / 6);
+
     for _ in 0..STEM_COUNT {
         let tip_pnt = Point {
             x: rng.random_range(5..max_x),
             y: rng.random_range(5..max_y)
         };
         let _ = renderer.draw_tile(tip_mat, &tip_pnt);
-        all_tips.push(tip_pnt);
+        branches.push(Branch{tip: tip_pnt});
     };
-    all_tips
+    branches
 }
